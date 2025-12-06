039e9f40d2a361ec31883927a6833a470b46b600
lafhielzakaria <lafhielzakaria@gmail.com>
2025-12-06 21:46:00 +0100

    train on drag and drop

--- train.js
+++ train.js
@@ -1,84 +1,18 @@
-const products = [
-    { name: "iPhone 14", category: "Electronics", price: 999, rating: 4.5, inStock: true },
-    { name: "MacBook Pro", category: "Electronics", price: 199, rating: 4.8, inStock: false },
-    { name: "Nike Shoes", category: "Fashion", price: 120, rating: 4.2, inStock: true },
-    { name: "Coffee Maker", category: "Home", price: 89, rating: 4.0, inStock: true },
-    { name: "Wireless Headphones", category: "Electronics", price: 199, rating: 4.6, inStock: true }
-];
-const priceRangeInput = document.getElementById("priceRange").addEventListener("input", () => {
-    priceValue.textContent = priceRange.value;
-    let filtredproductsContainer = "";
-    let filteredProductsByPrice = products.filter(product => product.price == priceRange.value)
-    for (item of filteredProductsByPrice) {
-        filtredproductsContainer +=
-            `
-        <p>${item.name}</p>
-        <p>${item.category}</p>
-        <p>${item.price}</p>
-        <p>${item.rating}</p>
-        <p>${item.inStock}</p>
-        `
-    }
-    productsContainer.innerHTML = filtredproductsContainer;
+    let box =   document.getElementById("dragBox");
+    let dropZone = document.getElementById("dropZone")
+   box.addEventListener("dragstart",(e)=>{
+    console.log(e.target.id)
+    e.target.style.opacity = "30%"    
 })
-if (categoryFilter.value == "") {
-    let filteredproductContainer = "";
-    for (item of products) {
-        filteredproductContainer +=
-            `
-          <p>${item.name}</p>
-          <p>${item.category}</p>
-          <p>${item.price}</p>
-          <p>${item.rating}</p>
-          <p>${item.inStock}</p>
-          `
-    }
-    productsContainer.innerHTML = filteredproductContainer;
+box.addEventListener("dragend",(e)=>{
+    e.target.style.opacity = "100%"    
 
-}
-categoryFilter.addEventListener("change", () => {
-    if (categoryFilter.value == "") {
-        let filteredproductContainer = "";
-        for (item of products) {
-            filteredproductContainer +=
-                `
-          <p>${item.name}</p>
-          <p>${item.category}</p>
-          <p>${item.price}</p>
-          <p>${item.rating}</p>
-          <p>${item.inStock}</p>
-          `
-        }
-        productsContainer.innerHTML = filteredproductContainer;
-        return;
-
-    }
-    let filteredproductContainer = "";
-    let filtredProductsByCategorie = products.filter(product => product.category == categoryFilter.value)
-    for (item of filtredProductsByCategorie) {
-        filteredproductContainer +=
-            `
-          <p>${item.name}</p>
-          <p>${item.category}</p>
-          <p>${item.price}</p>
-          <p>${item.rating}</p>
-          <p>${item.inStock}</p>
-          `
-    }
-    productsContainer.innerHTML = filteredproductContainer;
 })
-const productSearchInput = document.getElementById("productSearch").addEventListener('keyup', () => {
-    let searchedProduct = products.find(product => product.name == productSearch.value)
-    if (searchedProduct === undefined) {
-        productsContainer.innerHTML = "";
-        return;
-    }
-    productsContainer.innerHTML =
-        `
-      <p>${searchedProduct.name}</p>
-      <p>${searchedProduct.category}</p>
-      <p>${searchedProduct.price}</p>
-      <p>${searchedProduct.rating}</p>
-      <p>${searchedProduct.inStock}</p>
-      `
+dropZone.addEventListener("dragover",(e)=>{
+    e.preventDefault();
+    dropZone.appendChild(box)           
+})
+dropZone.addEventListener("dragend",(e)=>{
+    console.log("sss")
+  
 })
